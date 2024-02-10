2234c81056787392f8ac536881181f121410b9ef
Linh Nguyen <linh@linh-n.com>
2024-02-10 17:08:35 +0700

    update m3 code

--- magic-zoomcamp/data_exporters/export_to_gcs.py
+++ magic-zoomcamp/data_exporters/export_to_gcs.py
@@ -1,6 +1,9 @@
+from pandas import DataFrame
 import pyarrow as pa
 import pyarrow.parquet as pq
+import google.auth as ga
 import os
+
 
 
 if 'data_exporter' not in globals():
@@ -8,17 +11,19 @@ if 'data_exporter' not in globals():
 
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/main-shade-395414-2463a78bca4f.json'
+credentials = ga.load_credentials_from_file(
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+)
+os.environ['GOOGLE_CLOUD_PROJECT'] = credentials[1]
 
 @data_exporter
-def export_data_to_google_cloud_storage(data, *args, **kwargs) -> None:
-    project_id = kwargs['project_id']
+def export_data_to_google_cloud_storage(df: DataFrame, *args, **kwargs) -> None:
     bucket_name = kwargs['bucket_name']
     table_name = kwargs['table_name']
-    root_path = f"{bucket_name}-{project_id}/{table_name}"
-    print(root_path)
+    root_path = f"{bucket_name}/{table_name}"
     partition_cols = kwargs['partition_cols']
 
-    table = pa.Table.from_pandas(data)
+    table = pa.Table.from_pandas(df)
     gcs = pa.fs.GcsFileSystem()
     pq.write_to_dataset(
         table,
